0096b0011d89faef361f0209f062312dee6c6a0d
Dan Tracy <dant@aweber.com>
2014-04-25 17:32:51 -0400

    setup.py: Clean up the setup module and exclude tests

--- setup.py
+++ setup.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys
 
-options = {
-    "name"                  : "maxcdn",
-    "version"               : "0.0.3",
-    "description"           : "A Python REST Client for MaxCDN REST Web Services",
-    "author"                : "Joshua P. Mervine",
-    "author_email"          : "joshua@mervine.net",
-    "license"               : "MIT",
-    "keywords"              : "MaxCDN CDN API REST",
-    "packages"              : ['maxcdn'],
-    "url"                   : 'http://github.com/maxcdn/python-maxcdn'
-}
+install_requires = ['requests', 'requests_oauthlib', 'certifi']
+tests_require = ['nose']
 
-install_requires = [
-  "requests",
-  "requests_oauthlib",
-  "certifi"
-]
-tests_require = [
-  "nose",
-  "mock"
-]
-include_package_data = True
+if sys.version_info < (3, 2, 0):
+    tests_require.append('mock')
 
-try:
-    from setuptools import setup
-    options["install_requires"] = install_requires
-    options["include_package_data"] = include_package_data
-    options["tests_require"] = tests_require
-    setup(**options)
-
-except ImportError:
-    print("ERROR: setuptools wasn't found, please install it")
+if sys.version_info < (2, 7, 0):
+    tests_require.append('unittest2')
 
 
+setup(
+    name='maxcdn',
+    version='0.0.3',
+    description='A Python REST Client for MaxCDN REST Web Services',
+    author='Joshua P. Mervine',
+    author_email='joshua@mervine.net',
+    license='MIT',
+    keywords='MaxCDN CDN API REST',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    url='http://github.com/maxcdn/python-maxcdn',
+    include_package_data=True,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP'
+        'Topic :: Software Development :: Libraries',
+    ],
+)
